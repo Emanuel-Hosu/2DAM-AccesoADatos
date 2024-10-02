a59b5c59ea8ffd340617c31e9c666ae3ca05a7f4
Emisito <131873705+emisito@users.noreply.github.com>
2024-10-02 17:41:22 +0200

    Updated HashMaps and Adding 2 methods 4 BST trees

--- HashTables/AgendaTelefonica/src/PhoneAgend/module-info.java
+++ HashTables/AgendaTelefonica/src/PhoneAgend/module-info.java
@@ -1,8 +1,3 @@
-/**
- * 
- */
-/**
- * 
- */
+
 module AgendaTelefonica {
 }
---
+++ Trees/BST/src/BSTTree/Node.java
@@ -0,0 +1,13 @@
+package BSTTree;
+
+public class Node {
+	int value;
+	Node right;
+	Node left;
+	
+	public Node(int _value) {
+		value = _value;
+		right = null;
+		left = null;
+	}
+}
